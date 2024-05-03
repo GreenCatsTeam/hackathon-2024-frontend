9a529801b437d78163b5d5050a8a36b55ed3c5fc
Alex Towny <grazhdanov3002@mail.ru>
2024-05-04 00:22:58 +0400

    add location recognition

--- src/components/screens/admin-verify-cards/map/Map.js
+++ src/components/screens/admin-verify-cards/map/Map.js
@@ -1,17 +1,75 @@
-import { YMaps, Map, RoutePanel, Clusterer, Placemark , Panorama} from "@pbe/react-yandex-maps";
-
+import { YMaps, Map,Placemark ,Clusterer} from "@pbe/react-yandex-maps";
+import React, { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button'
 
 const MapView = () => {
+    const [myPosition, setMyPosition] = useState({latitude:50.0,longitude:50.0});  
+    const [mapInstance, setMapInstance] = useState(null)
+    let trash = [
+      {latitude:50.0,longitude:55,},
+      {latitude:55.0,longitude:55,},
+      {latitude:59.0,longitude:50,}
+    ];
     
+    useEffect(() => {
+      const getLocation = async () => {
+        try {
+          if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(success);
+          } else {
+            console.log("Geolocation not supported");
+          }
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+  
+      getLocation();
+    }, []);
+    
+    function success(pos) {
+      const crd = pos.coords;
+      setMyPosition({latitude:crd.latitude, longitude:crd.longitude})  
+    };
+
+    useEffect(() => {
+      const setLocation = async () => {
+        if (!(mapInstance === null))
+          mapInstance.setCenter([ myPosition.latitude, myPosition.longitude])
+          
+      }
+      setLocation();
+    }, [myPosition, mapInstance])
+  
+
+
   
     return (
+      
         <YMaps>
-          <Map height={500} width={'auto'} defaultState={{ center: [55.75, 37.57], zoom: 9 }} >
-            <RoutePanel options={{ float: "right" }} />
+          <Map
+          instanceRef={(instance) => {setMapInstance(instance)}}
+          height={500} 
+          width={+'auto'} 
+          defaultState={{ center: [myPosition.longitude, myPosition.latitude], zoom: 12 }} >
+          <Clusterer
+          options={{
+            preset: "islands#invertedVioletClusterIcons",
+            groupByCoordinates: false,
+            clusterDisableClickZoom: true,
+            clusterHideIconOnBalloonOpen: false,
+            geoObjectHideIconOnBalloonOpen: false
+          }}
+        >
+          {trash.map((place) => (<Placemark defaultGeometry={[place.longitude, place.latitude]} />))}
+          
+          </Clusterer>
           </Map>
+          
       </YMaps>
     );
   };
   
   export default MapView;
   //<Panorama defaultPoint={[55.733685, 37.588264]} />
+  //<RoutePanel options={{ float: "right" }} />
